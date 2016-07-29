470bcc1d22249297c91ecd0d18bba377024eaad4
nathan Vieira <nathanvmag@gmail.com>
2016-07-29 12:29:32 -0300

    Numeros primos

---
+++ Familia primos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> primos = new List<int>();
+            Console.WriteLine("Até que número você gostaria de saber quais são os primos?");
+            int qnts = int.Parse(Console.ReadLine()); 
+
+            for (int i = 2; i<qnts; i++)
+            {
+                primos.Add(i);               
+            }
+
+            for (int i = 0; i < primos.Count; i++)
+            {
+                if (i > 1 && primos.Contains(i))
+                {
+                    for( int v = i; v<primos.Count; v++)
+                    {
+                        primos.Remove(v * i);
+                    }
+                }
+            }
+
+            for (int i = 0; i < primos.Count; i++)
+            {
+                Console.WriteLine("O " + (i+1) + "º número primo é " + primos[i]);
+            }
+               
+            Console.ReadLine();
+        }
+    }
+}
